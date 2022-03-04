cbd8bd77228eb75f62ce49e6ad1c7ed02ffc89bf
Shadowstar <68189386+ShadowstarUnited@users.noreply.github.com>
2022-03-04 17:20:00 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,24 +7,23 @@ import sys
 from nextcord.ext import tasks
 from itertools import cycle
 
-bot = commands.AutoShardedBot(command_prefix=['!'], intents=nextcord.Intents.all(), help_command=None, case_insensitive=True, owner_ids=[Your UserID])
-bot.remove_command('help')
+bot = commands.AutoShardedBot(command_prefix=['!'], intents=nextcord.Intents.all(), help_command=None, case_insensitive=True, owner_ids=[Your UserID]) #Paste your Discord UserID here
+bot.remove_command('help') #removes the built in help command to allow the custom paginated one to work
 
 path = os.getcwd()
 directory = f'{path}/cogs/'
 files_in_directory = os.listdir(directory)
-
 cogs = [f'cogs.{file[:-3]}' for file in files_in_directory if file.endswith(".py")]
+
+bot.load_extension('jishaku')
+for cog in cogs:
+    bot.load_extension(cog)
+    
 @bot.event
 async def on_ready():
-
     members = 0
     for guild in bot.guilds:
         members += guild.member_count - 1
-
-    bot.load_extension("jishaku")
-    for cog in cogs:
-        bot.load_extension(cog)
     print(f'\nLogged in as: {bot.user.name}')
     await bot.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.watching, name=f"{members} members"))
 
