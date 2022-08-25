ae12a6dac3d71bc769daada0e72338f04e4edc7c
Shadowstar <68189386+ShadowstarUnited@users.noreply.github.com>
2022-08-25 20:49:42 +0100

    Update database.py

--- database.py
+++ database.py
@@ -5,3 +5,4 @@ db = AsyncIOMotorClient(tokens.mongo)
 BotDB = db["botdb"] # These can be named anything, but if you DO rename BotDB, make sure it's referenced everywhere the database is called.
 BlacklistCol: Collection = BotDB["blacklist"]
 PrefixCol: Collection = BotDB["prefixes"]
+# This is your DataBase. Whenever you'd like to create a new type of collection, it goes in here. PrefixCol = prefix collection. This is where the database stores custom prefixes. BlacklistCol = blacklist collection, which is where the database stores blacklisted servers or users.
