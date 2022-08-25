be242ca720dfb42fd884be43c9dbc187876f7aaa
Shadowstar <68189386+ShadowstarUnited@users.noreply.github.com>
2022-08-25 20:43:51 +0100

    Update example-tokens.py

--- example-tokens.py
+++ example-tokens.py
@@ -1,4 +1,4 @@
 # Rename this file to tokens.py
 bot = "" #This is your Bot Token
-mongo = "mongodb+srv://YourName:Token@clusterName" #This is your MongoDB Token, if you do not want a DataBase, you can delete this line
+mongo = "mongodb+srv://YourName:Token@clusterName" #This is your MongoDB Token, if you do not want a DataBase, this bot template is not for you.
 # I will not make a guide on how to create a MongoDB or set it up, you will need to do this by yourself.
