0e74d8b92101890e3ba42ec348df2b788d25ec8c
Shadowstar <68189386+ShadowstarUnited@users.noreply.github.com>
2022-08-25 20:42:45 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,44 @@
+from typing import Optional
 import nextcord
+import tokens
 import os
-from nextcord.ext import commands
-import random
-import traceback
-from nextcord.ext import tasks
+from nextcord.ext import commands, tasks
 from itertools import cycle
+from database import PrefixCol
 
-bot = commands.AutoShardedBot(command_prefix=['!'], intents=nextcord.Intents.all(), help_command=None, case_insensitive=True, owner_ids=[Your UserID]) #Paste your Discord UserID here
-bot.remove_command('help') #removes the built in help command to allow the custom paginated one to work
+intents=nextcord.Intents.all()
+
+
+async def get_prefix(bot: commands.Bot, message: nextcord.Message):
+    guild: Optional[nextcord.Guild] = message.guild
+    if guild is None:
+        return
+    x = await PrefixCol.find_one({"guild": f"{guild.id}"})
+    if x is None:
+        return commands.when_mentioned_or("d!")(bot, message)
+    elif x is not None:
+        custom_prefix = x["prefix"]
+        return commands.when_mentioned_or(custom_prefix)(bot, message)
+
+
+bot = commands.AutoShardedBot(
+    case_insensitive=True, intents=intents, command_prefix=get_prefix
+)
+bot.remove_command("help")
+bot.owner_ids = [
+    000000000000000000,
+]  # Your Discord UserID. You can put multiple UserID's, but make sure they're on a separate line, with a , at the end.
 
 path = os.getcwd()
-directory = f'{path}/cogs/'
+directory = f"{path}/cogs/"
 files_in_directory = os.listdir(directory)
-cogs = [f'cogs.{file[:-3]}' for file in files_in_directory if file.endswith(".py")]
+cogs = [f"cogs.{file[:-3]}" for file in files_in_directory if file.endswith(".py")]
 
-bot.load_extension('jishaku')
+bot.load_extension("jishaku")
 for cog in cogs:
     bot.load_extension(cog)
-    
+
+
 @bot.event
 async def on_ready():
     members = 0
@@ -26,4 +47,6 @@ async def on_ready():
     print(f'\nLogged in as: {bot.user.name}')
     await bot.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.watching, name=f"{members} members"))
 
-bot.run('Your Bot Token')
+
+if __name__ == "__main__":
+    bot.run(tokens.bot)
