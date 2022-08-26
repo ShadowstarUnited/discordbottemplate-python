a817c88d0d337865d25d6cec1a1b2eca3c129b73
Shadowstar <68189386+ShadowstarUnited@users.noreply.github.com>
2022-08-26 18:10:06 +0100

    fix usercol thanks to Envy!#0001

--- database.py
+++ database.py
@@ -5,4 +5,5 @@ db = AsyncIOMotorClient(tokens.mongo)
 BotDB = db["botdb"] # These can be named anything, but if you DO rename BotDB, make sure it's referenced everywhere the database is called.
 BlacklistCol: Collection = BotDB["blacklist"]
 PrefixCol: Collection = BotDB["prefixes"]
+UserCol: Collection = DrunkDB["users"]
 # This is your DataBase. Whenever you'd like to create a new type of collection, it goes in here. PrefixCol = prefix collection. This is where the database stores custom prefixes. BlacklistCol = blacklist collection, which is where the database stores blacklisted servers or users.
